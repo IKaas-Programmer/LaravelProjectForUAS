99af73b432bd5146aa5e68f30744a226d440f1a6
IKaas <150572441+IKaas-Programmer@users.noreply.github.com>
2025-04-19 00:22:58 +0800

    Add Articles_comments

---
+++ app/Filament/Resources/ArticlesCommentsResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ArticlesCommentsResource\Pages;
+use App\Filament\Resources\ArticlesCommentsResource\RelationManagers;
+use App\Models\Articles_comments;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+
+class ArticlesCommentsResource extends Resource
+{
+    protected static ?string $model = Articles_comments::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('article_id')
+                    ->label('Articles')
+                    ->relationship('article', 'title'),
+                
+                Textarea::make('comment')
+                    ->label('Comment')
+                    ->placeholder('Comment')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('Article.title')
+                    ->label('Article Title'),
+                TextColumn::make('comment')
+                    ->label('Comment')
+                    ->limit(50),
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(),
+                
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticlesComments::route('/'),
+            'create' => Pages\CreateArticlesComments::route('/create'),
+            'edit' => Pages\EditArticlesComments::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Articles_comments.php
+++ app/Models/Articles_comments.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Articles_comments extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'article_id',
+        'comment',
+        
+    ];
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+    public function article()
+    {
+        return $this->belongsTo(Articles::class, 'article_id');
+    }
 }
