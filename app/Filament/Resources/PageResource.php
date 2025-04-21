6a455dc2e14b9ea192d606020e99da8db57db3cc
IKaas <150572441+IKaas-Programmer@users.noreply.github.com>
2025-04-21 22:15:02 +0800

    adjust Page

--- app/Filament/Resources/PageResource.php
+++ app/Filament/Resources/PageResource.php
@@ -5,13 +5,21 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PagesResource\Pages;
 use App\Filament\Resources\PagesResource\RelationManagers;
 use App\Models\Page;
+use Dom\Text;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Columns\ColorColumn;
 
 class PageResource extends Resource
 {
@@ -23,7 +31,29 @@ class PageResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('title')
+                    ->label('Title')
+                    ->placeholder('Title')
+                    ->required(),
+                Select::make('type')
+                    ->label('Type')
+                    ->options([
+                        'page' => 'Page',
+                        'post' => 'Post',
+                        'article' => 'Article',
+                        'blog' => 'Blog',
+                        'review' => 'Review',
+                    ])
+                    ->placeholder('Options')
+                    ->required(),
+                    ColorPicker::make('color')
+                    ->label('Color')
+                    ->placeholder('RGB Color')
+                    ->rgb(),
+                Textarea::make('description')
+                    ->label('Description')
+                    ->placeholder('Description')
+                    ->required(),
             ]);
     }
 
@@ -31,13 +61,21 @@ class PageResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title'),
+                TextColumn::make('description')
+                    ->limit(50),
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()    
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Page extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'type',
+        'color',
+    ];
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
