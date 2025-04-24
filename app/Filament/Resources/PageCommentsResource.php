71461168ef2dcc234092238b64609caab9a76c10
IKaas <150572441+IKaas-Programmer@users.noreply.github.com>
2025-04-24 23:01:14 +0800

    Add PageComments and Adjust litte bit

--- app/Filament/Resources/PageCommentsResource.php
+++ app/Filament/Resources/PageCommentsResource.php
@@ -2,10 +2,11 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\ArticlesResource\Pages;
-use App\Filament\Resources\ArticlesResource\RelationManagers;
-use App\Models\Articles;
+use App\Filament\Resources\PageCommentsResource\Pages;
+use App\Filament\Resources\PageCommentsResource\RelationManagers;
+use App\Models\page_comments;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -14,12 +15,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\RichEditor;
 use Filament\Tables\Columns\TextColumn;
 
-
-class ArticlesResource extends Resource
+class PageCommentsResource extends Resource
 {
-    protected static ?string $model = Articles::class;
+    protected static ?string $model = page_comments::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
@@ -27,32 +28,33 @@ class ArticlesResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('title')
-                    ->label('Title')
-                    ->placeholder('Title')
+                Select::make('page_id')
+                    ->label('Page')
+                    ->relationship('page', 'title')
                     ->required(),
-                Textarea::make('description')
-                    ->label('Description')
-                    ->placeholder('Description')
+                Textarea::make('comment')
+                    ->label('Comment')
+                    ->placeholder('Comment')
                     ->required(),
             ]);
+            
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('title')
-                    ->label('Title'),
-                TextColumn::make('description')
-                    ->label('Description'),
+                TextColumn::make('Page.title')
+                ->label('Pages Title'),
+                TextColumn::make('comment')
+                ->label('Comment')
+                ->limit(50),//
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -71,9 +73,9 @@ class ArticlesResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListArticles::route('/'),
-            'create' => Pages\CreateArticles::route('/create'),
-            'edit' => Pages\EditArticles::route('/{record}/edit'),
+            'index' => Pages\ListPageComments::route('/'),
+            'create' => Pages\CreatePageComments::route('/create'),
+            'edit' => Pages\EditPageComments::route('/{record}/edit'),
         ];
     }
 }
--- app/Models/page_comments.php
+++ app/Models/page_comments.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class page_comments extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'page_id',
+        'comment',
+        
+    ];
+    public function page()
+    {
+        return $this->belongsTo(Page::class);
+    }
 }
