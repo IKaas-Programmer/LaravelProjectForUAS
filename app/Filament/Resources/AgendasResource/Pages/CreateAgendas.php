9f51eecf440d9fcf143e94ee611832292835aecf
ygs-py <ikaasthea@gmail.com>
2025-05-07 23:49:34 +0800

    Add MutateFormData Agendas

--- app/Filament/Resources/AgendasResource/Pages/CreateAgendas.php
+++ app/Filament/Resources/AgendasResource/Pages/CreateAgendas.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAgendas extends CreateRecord
 {
     protected static string $resource = AgendasResource::class;
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+
+        return $data;
+    }
 }
