f418d4adcff52f54729b8d303f5611f7b3faf35a
ygs-py <ikaasthea@gmail.com>
2025-05-08 12:12:12 +0800

    update index

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -6,6 +6,7 @@
     <title>Laravel Project UAS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
+
 <body>
 <header>
     @include('layout.navbar')
@@ -20,6 +21,8 @@
 <footer>
 
 </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    
 </body>
 </html>
