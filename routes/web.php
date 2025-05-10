896e53321b2ab8ef1f1108ececd5afb6a3f82de3
IKaas <artasuteja143@gmail.com>
2025-05-10 22:12:55 +0800

    Update 1.1 - "add route api"

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgendaController;
 
 // Route::get('/', function () {
 //    return view('welcome');
 // });
 Route::get('/',[HomeController::class,'index'])->name('home');
+
+Route::get('/api',[HomeController::class,'api'])->name('api');
