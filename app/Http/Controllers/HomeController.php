5f63799af992d21fd83568f5e80bd5444a7ccdbb
IKaas <artasuteja143@gmail.com>
2025-05-10 22:11:19 +0800

    update1.1 - "Add API"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Articles;
+use Illuminate\Support\Facades\Http;
+use Filament\Forms\Get;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -16,5 +18,16 @@ class HomeController extends Controller
         ];
 
         return view('article', $data);
+    }
+
+    public function api() {
+        $response = Http::get('https://api.escuelajs.co/api/v1/users');
+
+        if ($response->successful()) {
+            $data = $response->object();
+        } else {
+        $data = [ 'message' => 'Error fetching data from API'];
+        };
+        return view('api',compact('data'));
     }
 }
